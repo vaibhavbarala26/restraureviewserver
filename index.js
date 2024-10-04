73d469a51b3b7a2e8ea1bcb04f60ab9c4087c07d
Vaibhav Barala <vaibhavbarala8@gmail.com>
2024-10-04 11:45:44 +0530

    second

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ app.use(express.json())
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    methods:"GET , POST , PUT , DELETE",
-    credentials:true,
-}))
+    origin: ["http://localhost:5173", "https://restraureview.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+
 app.use(cookiParser(process.env.SECRET_TOKEN_KEY))
 
 
